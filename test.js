b8ecbfbcc7a61c21dea9e7be57b8b4d2d11a0698
YT <yongtao.fan@yunshipei.com>
2017-04-24 16:25:37 +0800

    fix test error color

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const err = new Error('i am error')
 l.l('log: %s', 'world');
 l.i('info, %s-%d', 'info', 100);
 l.w('waring: %d', 10);
-l.e('err', err.message);
+l.e('err: %s', err.message);
 l.t('trace');
 l.d({dir: 'YT'});
 l.d('Hello, %s', 'YT');
