dd8b07e05d8545854a57717ff31808cedc154e00
YT <yongtao.fan@yunshipei.com>
2017-04-24 23:47:08 +0800

    add line method filename to logger

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * lyg
  * CopyRight (c) 2017 YT <tate_fan@163.com>
@@ -7,23 +6,77 @@
 const slice = Array.prototype.slice;
 const toStr = Object.prototype.toString;
 const chalk = require('chalk');
+const path = require('path');
 
 /**!
  * get color function
+ * @private
  */
-const colorFn = (method, color) => chalk[color[method]];
-const isObject = (obj) => toStr.call(obj) === '[object Object]';
+var colorFn = (method, color) => chalk[color[method]];
+var isObject = (obj) => toStr.call(obj) === '[object Object]';
+
 
 /**!
  * add meta information
+ * @private
  */
-const format = (method, msg) => {
-  let time = new Date();
-  return `[${time}]-[${method}]-msg: ${msg}`;
+var format = (level, msg, opts) => {
+  opts = opts || {};
+  let {line, method, file} = opts;
+  method = /anonymous/ig.test(method)? 'anonymous': method;
+  let time = new Date().toLocaleString();
+  return `[${time}]-[${level}]-[${file}]-[${line}]-[${method}]-msg: ${msg}`;
 };
 
 
-const methods = [ 'log', 'info', 'warn', 'error', 'dir', 'trace' ];
+/**!
+ * @desc get call stack info
+ * @private
+ */
+var getStackInfo = function() {
+  // https://github.com/v8/v8/wiki/Stack%20Trace%20API
+  let data = {};
+  let reg1 = /at\s+(.*)\s+\((.*):(\d*):(\d*)\)/i;
+  let reg2 = /at\s+()(.*):(\d*):(\d*)/i;
+
+  let list = (new Error()).stack.split('\n').slice(3);
+  let s = list[0];
+  let sp = reg1.exec(s) || reg2.exec(s);
+  if (sp && sp.length === 5) {
+    data.method = sp[1];
+    data.path = sp[2];
+    data.line = sp[3];
+    data.file = path.basename(data.path);
+  }
+  return data;
+}
+
+
+/**
+ * @desc override console method
+ */
+const methods = ['log', 'info', 'warn', 'error', 'dir', 'trace'];
+methods.forEach((method) => {
+  var char = method.charAt(0).toLowerCase();
+  exports[char] = function() {
+    let args = slice.call(arguments);
+    let opts = getStackInfo();
+    let cFn = colorFn(method, colorMap);
+    if ('dir' === method) {
+      if (isObject(args[0])) args[0] = JSON.stringify(args[0]).replace('\n', ' ');
+      args[0] = cFn(format(method, cFn(args[0]), opts));
+      console.log.apply(console, args);
+    } else {
+      args[0] = cFn(format(method, cFn(args[0]), opts));
+      console[method].apply(console, args);
+    }
+  };
+});
+
+
+/**
+ * @desc default color setting
+ */
 const colorMap = {
   log:   'blue',
   info:  'cyan',
@@ -32,19 +85,3 @@ const colorMap = {
   trace: 'magenta',
   dir:   'green'
 };
-
-methods.forEach((method) => {
-  var char = method.charAt(0).toLowerCase();
-  exports[char] = function() {
-    let args = slice.call(arguments);
-    let cFn = colorFn(method, colorMap);
-    if ('dir' === method) {
-      if (isObject(args[0])) args[0] = JSON.stringify(args[0]);
-      args[0] = cFn(format(method, cFn(args[0])));
-      console.log.apply(console, args);
-    } else {
-      args[0] = cFn(format(method, cFn(args[0])));
-      console[method].apply(console, args);
-    }
-  };
-});
--- test.js
+++ test.js
@@ -1,11 +1,33 @@
 'use strict';
-const l = require('./index');
-const err = new Error('i am error')
 
-l.l('log: %s', 'world');
-l.i('info, %s-%d', 'info', 100);
+const l = require('./index');
+const util = require('util');
+const path = require('path');
+const err = new Error('i am error')
+const obj = {
+  name: 'YT',
+  test: true,
+  num: 10,
+  date: new Date()
+};
+
+l.l('coming anonymous');
+l.i('multi value - %s:%d ', 'info', 100);
 l.w('waring: %d', 10);
-l.e('err: %s', err.message);
-l.t('trace');
-l.d({dir: 'YT'});
-l.d('Hello, %s', 'YT');
+l.e('%s', err.message);
+l.d(obj);
+l.d('Hello, %s | %s \n', 'YT', 'string');
+
+
+function getInfo() {
+  l.i('coming getInfo');
+  l.l('multi: %s | %s', 'getInfo', true);
+  l.w('waring: %d', 10);
+  l.e('%s', err.message);
+  l.d(obj);
+  l.d('Hello, %s | %s \n', 'YT', 100);
+}
+
+getInfo();
+
+l.t('the trace info');
